461a50a5b40ac70c9fdd0dce9d5779b44da71ac1
Natanael-coder <Milewski.natanael@gmail.com>
2024-12-11 09:57:56 +0100

    mo

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String wpisane;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj typek, napisz coś lmao \n");
+        wpisane = scanner.nextLine();
+        System.out.println("No mordo, wpisałeś: " + wpisane);
+    }
+}
